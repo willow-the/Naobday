1b78eed734cef88f5946ad28a89818f0c4f8c544
willow_the <willowlin2004@gmail.com>
2017-08-19 11:07:14 -0500

    I'm pretty much finished

--- script.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  function showHex(){
-          var hex =  document.querySelector("#color_hex_value"),
-              body = document.querySelector("body");
-
-          body.style.backgroundcolor = document.querySelector("#color_picker").value;
-        }
-});
